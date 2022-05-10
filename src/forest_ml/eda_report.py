9d80ae513c5a44d9ac4f3504eb64686de0720862
EvgenVer <evgenyveremiev88@gmail.com>
2022-05-10 18:18:16 +0500

    add script for pandas profiling

---
+++ src/forest_ml/eda_report.py
@@ -0,0 +1,34 @@
+from email.policy import default
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+from pathlib import Path
+from joblib import dump
+import click
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/report.html",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True, 
+)
+
+def eda_report(
+    dataset_path: Path,
+    save_model_path: Path
+) -> None:
+    df = pd.read_csv(dataset_path)
+    profile = ProfileReport(df, title="Profiling report", explorative=True)
+    profile.to_file(save_model_path)
+    
+
+
